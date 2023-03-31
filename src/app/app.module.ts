5fa887c10061d3229a0df2f7120060c339c7dd6d
Shailesh  Thakur <shaileshthakur94@gmail.com>
2023-03-31 22:57:21 +0530

    added routing for contacts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddContactComponent } from './contact/add-contact/add-contact.component';
+import { EditContactComponent } from './contact/edit-contact/edit-contact.component';
+import { ListContactComponent } from './contact/list-contact/list-contact.component';
+import { Routes } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router'
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path:'contacts', component: ListContactComponent},
+  {path:'contacts/add', component:AddContactComponent},
+  {path:'contacts/edit/:id', component:EditContactComponent},
+  {path:'**', redirectTo:''}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddContactComponent,
+    EditContactComponent,
+    ListContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
